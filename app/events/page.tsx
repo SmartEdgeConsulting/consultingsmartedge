6900f71a9a636b8df3e3ab255a3d2ee6aae79383
Oloniyo-Bolaji <ayoolabolaji12@yahoo.com>
2025-11-19 20:39:15 +0100

    feat: add Blog Content Page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
-  return <div></div>;
+const EventsPage = () => {
+  return <main></main>;
 };
 
-export default page;
+export default EventsPage;
