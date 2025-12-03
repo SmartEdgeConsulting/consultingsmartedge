f7efec5eec1159844eac93b451b2ea778dd349bf
Oloniyo-Bolaji <ayoolabolaji12@yahoo.com>
2025-12-03 16:32:08 +0100

    fix build error

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -2,16 +2,16 @@
 import { createClient } from "@sanity/client";
 
 export const serverClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.SANITY_PROJECT_ID!,
+  dataset: process.env.SANITY_DATASET!,
   apiVersion: "2025-11-18", // Use current date or your preferred API version
   useCdn: false, // Disable CDN for server-side operations
   token: process.env.SANITY_API_TOKEN, // Use server-side token
 });
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.SANITY_PROJECT_ID!,
+  dataset: process.env.SANITY_DATASET!,
   apiVersion: "2025-11-18",
   useCdn: true, // Enable CDN for faster reads
 });
