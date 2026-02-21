cf839aa97bb9c71af96663d729fc6e3cbf2f0d52
Oloniyo-Bolaji <ayoolabolaji12@yahoo.com>
2026-02-21 10:09:26 +0100

    fix: fix build error

--- app/(dashboard)/dashboard/uploads/page.tsx
+++ app/(dashboard)/dashboard/uploads/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const UploadsPage = () => {
-  return (
+  return (  
      <main className="min-h-screen flex justify-center items-center p-4">
       Coming soon...
     </main>
